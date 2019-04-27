f4c079f798637d5155adcc4d3bde9315baae425d
saurabh kumar <saurabh.kumar07@sap.com>
2019-04-27 19:24:14 +0530

    Created basic headers

---
+++ shree-web/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+import Header from "../component/Header"
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <Header />
+            </div>)
+    }
+}
---
+++ shree-web/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import Header from "./component/Header"
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <Header />
+            </div>)
+    }
+}
+export default App
