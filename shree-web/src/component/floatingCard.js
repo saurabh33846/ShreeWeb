f63b21331ccadb497edd0e26dd5a86cb14354543
saurabh kumar <saurabh.kumar07@sap.com>
2019-06-11 11:14:13 +0530

    modified css of floating banner to make it responsive

--- shree-web/src/component/floatingCard.js
+++ shree-web/src/component/floatingCard.js
@@ -2,26 +2,29 @@ import React from 'react'
 const FloatingBanner = (props) => {
     return (
         <div>
-            <div className= "floatingCardContainer">
-            <div className = "companyName">
-                <span>Orbit</span>
-                <span style={{"letter-spacing": "-3px"}}>
-                    <span style={{"color": "blue"}} >P</span>
-                    <span style={{"color":"sky"}}>r</span>
-                    <span style={{"color":"green"}}>i</span>
-                    <span style={{"color":"yellow"}}>n</span>
-                    <span style={{"color":"orange"}}>t</span>
-                    <span style={{"color":"red"}}>s</span>
-                </span>
+            <div className="floatingCardContainer">
+                <div className="companyName">
+                    <span>Orbit</span>
+                    <span style={{ "letter-spacing": "-3px" }}>
+                        <span style={{ "color": "blue" }} >P</span>
+                        <span style={{ "color": "sky" }}>r</span>
+                        <span style={{ "color": "green" }}>i</span>
+                        <span style={{ "color": "yellow" }}>n</span>
+                        <span style={{ "color": "orange" }}>t</span>
+                        <span style={{ "color": "red" }}>s</span>
+                    </span>
 
-                <p className = "quote">Discover..Develop..Deliver!</p>
+                    <div className="quote">Discover..Develop..Deliver!</div>
+                </div>
+
+                <div className = "seperator">
+                </div>
+                <div className="quote2">
+                    <span style={{ "font-size": "35px;" }}>"</span> Your best  printing  partner!
+        </div>
             </div>
-            <div className= "quote2">
-                <span style={{"font-size":"35px;"}}>"</span> Your best  printing  partner!
         </div>
-        </div>
-        </div>
-        
+
     );
 }
 
